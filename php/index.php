08ecf2ed287976d116b2f0c8e8bd66ad8e4f5d66
Eduardo Araújo <110600044+araujoedu7@users.noreply.github.com>
2023-12-16 14:09:22 -0300

    Update index.php

--- php/index.php
+++ php/index.php
@@ -26,21 +26,78 @@
 
 <body>
 
-    <form>
+    <form  method="POST" action="envia.php">
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Usuario</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"  name="user">
             <div id="emailHelp" class="form-text">Nunca compartilharemos seus dados com mais ninguém.</div>
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Senha</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
+            <input type="password" class="form-control" id="exampleInputPassword1"  name="senha">
         </div>
         <div class="mb-3 form-check">
             <input type="checkbox" class="form-check-input" id="exampleCheck1">
             <label class="form-check-label" for="exampleCheck1">Li e aceito os <a href="">Termos e condições</a></label>
         </div>
-        <button type="submit" class="btn btn-primary">Enviar</button>
+        <button name="enviar" type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+</body>
+
+
+
+<style>
+
+    a{
+        text-decoration: none;
+    }
+</style>
+
+
+
+</html><!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+    lin
+</head>
+
+<body>
+
+</body>
+
+</html>
+
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <form  method="POST" action="envia.php">
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Usuario</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"  name="user">
+            <div id="emailHelp" class="form-text">Nunca compartilharemos seus dados com mais ninguém.</div>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Senha</label>
+            <input type="password" class="form-control" id="exampleInputPassword1"  name="senha">
+        </div>
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Li e aceito os <a href="">Termos e condições</a></label>
+        </div>
+        <button name="enviar" type="submit" class="btn btn-primary">Enviar</button>
     </form>
 </body>
 
